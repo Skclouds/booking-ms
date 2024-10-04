9ed21033ea81e810fe40f32142b0d4eb9d3a7284
Skclouds <kaushalsingh1715@gmail.com>
2024-10-04 13:40:24 +0530

    code commit

---
+++ src/main/java/com/example/booking_ms/hotel.java
@@ -0,0 +1,14 @@
+package com.example.booking_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hotel {
+    @GetMapping("/hotel")
+
+
+    public String getData() {
+        return"Please book your ticket in benguluru 49% discount";
+    }
+}
